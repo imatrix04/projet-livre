89dd7371edcb541f027b0a937c0aafc53a981d67
Nicolas De Falco <100281343+NicolasDeFalco@users.noreply.github.com>
2025-12-16 11:33:23 +0100

    Catalogue: Created catalogue component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,42 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   title: string = 'Bibliothèque Numérique';
-  searchTerm: string = '';
-
-  books = [
-    {
-      title: '1984',
-      author: 'George Orwell',
-      year: 1949,
-      available: true,
-      image: 'assets/images/1984_Orwell.jpg'
-    },
-    {
-      title: 'Le Seigneur des Anneaux',
-      author: 'J.R.R. Tolkien',
-      year: 1954,
-      available: false,
-      image: 'assets/images/LeSeigneurDesAnneaux_Tolkien.jpg'
-    },
-    {
-      title: 'L’Étranger',
-      author: 'Albert Camus',
-      year: 1942,
-      available: true,
-      image: 'assets/images/Letranger_Camus.jpg'
-    }
-  ];
-
-  get filteredBooks() {
-    if (!this.searchTerm) {
-      return this.books;
-    }
-    console.log(this.searchTerm);
-    
-    return this.books.filter(book =>
-      book.title.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
-      book.author.toLowerCase().includes(this.searchTerm.toLowerCase())
-    );
-  }
-
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { CatalogueComponent } from './catalogue/catalogue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogueComponent
   ],
   imports: [
     BrowserModule,
