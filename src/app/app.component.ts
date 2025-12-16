0708631301e333575fb39168600bedc5b2a6e53a
imatrix04 <florent.germain040105@orange.fr>
2025-12-16 09:44:06 +0100

    premiere base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'front';
+
+  title = 'Bibliothèque Numérique';
+
+  books = [
+    {
+      title: '1984',
+      author: 'George Orwell',
+      year: 1949,
+      available: true,
+      image: 'assets/images/1984_Orwell.jpg'
+    },
+    {
+      title: 'Le Seigneur des Anneaux',
+      author: 'J.R.R. Tolkien',
+      year: 1954,
+      available: false,
+      image: 'assets/images/LeSeigneurDesAnneaux_Tolkien.jpg'
+    },
+    {
+      title: 'L’Étranger',
+      author: 'Albert Camus',
+      year: 1942,
+      available: true,
+      image: 'assets/images/Letranger_Camus.jpg'
+    }
+  ];
+
 }
