eee8167a02dcd07cee4203dd39116e5b2cfc9c10
imatrix04 <florent.germain040105@orange.fr>
2025-12-16 11:38:45 +0100

    update

--- src/app/new-book/new-book.component.ts
+++ src/app/new-book/new-book.component.ts
@@ -6,30 +6,72 @@ import { HttpClient, HttpEventType } from '@angular/common/http';
   templateUrl: './new-book.component.html',
   styleUrls: ['./new-book.component.scss']
 })
+
 export class NewBookComponent {
   book = {
     title: '',
     author: '',
-    year: null as number | null,
+    year: null,
     available: true,
-    image: ''
+    image: '' // on mettra le chemin reçu après upload
   };
 
-  message = '';
+  selectedFile: File | null = null;
+  imageName = '';
+  uploadedImageUrl: string | null = null;
 
   constructor(private http: HttpClient) {}
 
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.selectedFile = file;
+      // Si pas de nom personnalisé, on peut récupérer celui du fichier
+      if (!this.imageName) {
+        this.imageName = file.name;
+      }
+    }
+  }
+
+  uploadImage() {
+    if (!this.selectedFile || !this.imageName) {
+      alert('Sélectionnez un fichier et donnez-lui un nom.');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('image', this.selectedFile, this.imageName);
+
+    return this.http.post<{ imageUrl: string }>('http://localhost:3000/api/upload', formData);
+  }
+
   onSubmit() {
-    this.http.post('http://localhost:5000/api/books', this.book)
-      .subscribe({
-        next: (res) => {
-          this.message = 'Livre ajouté avec succès !';
-          this.book = { title: '', author: '', year: null, available: true, image: '' };
-        },
-        error: (err) => {
-          this.message = 'Erreur lors de l\'ajout du livre.';
-          console.error(err);
-        }
-      });
+    this.uploadImage()?.subscribe({
+      next: (res) => {
+        // imageUrl = chemin accessible sur le serveur
+        this.book.image = res.imageUrl;
+
+        // ensuite on poste le livre complet
+        this.http.post('http://localhost:3000/api/books', this.book)
+          .subscribe({
+            next: () => {
+              alert('Livre ajouté avec image !');
+              this.book = { title: '', author: '', year: null, available: true, image: '' };
+              this.selectedFile = null;
+              this.imageName = '';
+              this.uploadedImageUrl = null;
+            },
+            error: (err) => {
+              console.error(err);
+              alert('Erreur lors de l\'ajout du livre');
+            }
+          });
+        this.uploadedImageUrl = res.imageUrl;
+      },
+      error: (err) => {
+        console.error(err);
+        alert('Erreur upload image');
+      }
+    });
   }
 }
